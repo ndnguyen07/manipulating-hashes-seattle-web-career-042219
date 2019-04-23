5ce3223d111b078724dddcb52e40152275aa2f09
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-23 21:42:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-    hash.each do |names, icecream|
+    contacts.each do |names, icecream|
       icecream(:favorite_icecream_flavors).delete_at ["strawberry"]
     end
 
